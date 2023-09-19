243d1c9df17273d32d346d455c7cce6be3b37aed
paula <paulactalim@gmail.com>
2023-09-19 15:48:46 -0300

    troca de tipo de menu e inclucao de FAB

--- lib/main.dart
+++ lib/main.dart
@@ -27,45 +27,103 @@ class _HomeState extends State<Home> {
   // Colocar nomes das telas na lista
   // final List<Widget> screens = [];
 
+  final List<Center> tabs = [
+    const Center(child: Text('home')),
+    const Center(child: Text('dispositivos')),
+    const Center(child: Text('dados')),
+    const Center(child: Text('login'))
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('BottomNavigationBar Sample'),
       ),
-      // body: screens[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        type: BottomNavigationBarType.fixed,
-        items: const [
-          BottomNavigationBarItem(
-            // icon: Icon(Icons.business),
-            icon: ImageIcon(
-              AssetImage("assets/images/sketch.png"),
-              size: 20,
-            ),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Aparelhos',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(FontAwesomeIcons.gamepad),
-            label: 'Dados',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'Login',
-          ),
-        ],
-        onTap: (int index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        selectedItemColor: Colors.amber[800],
+      body: tabs[_currentIndex],
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+        backgroundColor: Colors.amber,
+        foregroundColor: Colors.brown,
+        elevation: 10,
       ),
+      bottomNavigationBar: BottomAppBar(
+          shape: CircularNotchedRectangle(),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    _currentIndex = 0;
+                  });
+                },
+                icon: const Icon(Icons.home),
+              ),
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    _currentIndex = 1;
+                  });
+                },
+                icon: const Icon(Icons.home),
+              ),
+
+              //Espacamento entre icone e button action bar
+              const SizedBox(
+                width: 24,
+              ),
+
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    _currentIndex = 2;
+                  });
+                },
+                icon: const Icon(Icons.home),
+              ),
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    _currentIndex = 3;
+                  });
+                },
+                icon: const Icon(Icons.home),
+              ),
+            ],
+          )),
+      // bottomNavigationBar: BottomNavigationBar(
+      //   currentIndex: _currentIndex,
+      //   type: BottomNavigationBarType.fixed,
+      //   iconSize: 20,
+      //   items: const [
+      //     BottomNavigationBarItem(
+      //       // icon: Icon(Icons.business),
+      //       icon: Icon(FontAwesomeIcons.house),
+      //       label: 'Home',
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(FontAwesomeIcons.computer),
+      //       label: 'Dispositivos',
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(FontAwesomeIcons.house),
+      //       label: 'Dados',
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(FontAwesomeIcons.lock),
+      //       label: 'Login',
+      //     ),
+      //   ],
+      //   onTap: (int index) {
+      //     setState(() {
+      //       _currentIndex = index;
+      //     });
+      //   },
+      //   selectedItemColor: Colors.amber[800],
+      // ),
     );
   }
 }
