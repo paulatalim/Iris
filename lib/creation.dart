2021d31b3baab40bf92412a35e0b366a44e684a0
Pedro Mafra <pedro.m.vas@hotmail.com>
2023-09-20 20:33:15 -0300

    Criação de base da interface de cadastro
    Criação da base da interface utilizando collumn e body

--- lib/creation.dart
+++ lib/creation.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:iris_app/main.dart';
 import 'package:iris_app/loginmain.dart';
+import 'package:iris_app/main.dart';
 
 class SingIn extends StatelessWidget {
   const SingIn({super.key});
@@ -29,9 +29,55 @@ class _UserSingIn extends State<UserSingIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        /*
-      * TELA DE SING-IN
-      */
-        );
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(0XFF, 0x7C, 0x64, 0xEB),
+        title: Text(widget.title),
+        centerTitle: true,
+
+        //Menu de voltar ao menu (temporario)
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_rounded),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const TelaLogin()),
+            );
+          },
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          height: double.infinity,
+          width: double.infinity,
+          padding: const EdgeInsets.all(20),
+          child: Row(
+            //Vertical
+            crossAxisAlignment: CrossAxisAlignment.center,
+            //Horizontal
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: 'Nome:',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: 'Sobrenome:',
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
