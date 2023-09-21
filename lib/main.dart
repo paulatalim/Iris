38256ca29854045fec8ab0b999cffd3cdaae5845
paula <paulactalim@gmail.com>
2023-09-21 12:26:14 -0300

    melhora de codigo principal

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,34 @@ import 'package:flutter/material.dart';
 import 'configuracao.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    title: 'Iris',
+    debugShowCheckedModeBanner: false,
+    // theme: ThemeData(
+    //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    //   useMaterial3: true,
+    // ),
+    home: Configuracao(),
+  ));
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+// class MyApp extends StatefulWidget {
+//   const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
 
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Iris',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Configuracao(),
-    );
-  }
-}
+// class _MyAppState extends State<MyApp> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Iris',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         useMaterial3: true,
+//       ),
+//       home: const Configuracao(),
+//     );
+//   }
+// }
