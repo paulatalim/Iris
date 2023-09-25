d552ab892c63a217c2a66149f01c76ed597b52d0
paula <paulactalim@gmail.com>
2023-09-25 00:26:12 -0300

    diminuicao de classes

--- lib/main.dart
+++ lib/main.dart
@@ -3,30 +3,22 @@ import 'package:flutter/material.dart';
 import 'sobre.dart';
 
 void main() {
-  runApp(MyApp());
-}
+  Color minhaCorPersonalizada = Color(0xFF6A5ACD);
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    Color minhaCorPersonalizada = Color(0xFF6A5ACD); // Lavanda
-    // Substituído corClara por corLavandaClaro
-
-    return MaterialApp(
-      title: 'Iris',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(
-          primarySwatch: Colors.deepPurple, // Cor primária
-          accentColor: minhaCorPersonalizada, // Cor de destaque personalizada
-        ),
-        useMaterial3: true,
+  runApp(MaterialApp(
+    title: 'Iris',
+    theme: ThemeData(
+      colorScheme: ColorScheme.fromSwatch(
+        primarySwatch: Colors.deepPurple, // Cor primária
+        accentColor: minhaCorPersonalizada, // Cor de destaque personalizada
       ),
-      home: MyHomePage(
-        title: 'About us',
-        corLavanda: Color(0xFF6A5ACD),
-        corLavandaEscura: Color(0xFF483D8B),
-        corLavandaClaro: Color(0xFFB49CDC),
-      ),
-    );
-  }
+      useMaterial3: true,
+    ),
+    home: MyHomePage(
+      title: 'About us',
+      corLavanda: Color(0xFF6A5ACD),
+      corLavandaEscura: Color(0xFF483D8B),
+      corLavandaClaro: Color(0xFFB49CDC),
+    ),
+  ));
 }
