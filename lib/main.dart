ed1f3890ae6152c4d7b5b3e200bc9785ca24bcfb
paula <paulactalim@gmail.com>
2023-09-25 16:24:11 -0300

    menu funcional

--- lib/main.dart
+++ lib/main.dart
@@ -9,23 +9,17 @@ import 'sobre.dart';
 void main() {
   Color minhaCorPersonalizada = Color(0xFF6A5ACD);
 
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     title: 'Iris',
-    theme: ThemeData(
-      useMaterial3: true,
-      // Define the default brightness and colors.
-      colorScheme: ColorScheme.fromSwatch(
-        primarySwatch: Colors.deepPurple,
-        accentColor: minhaCorPersonalizada,
-      ),
-    ),
-    debugShowCheckedModeBanner: false,
-    home: const Menubar(),
-
-    // home: MyHomePage(
-    //   title: 'About us',
-    //   corLavandaEscura: Color(0xFF483D8B),
-    //   corLavandaClaro: Color(0xFFB49CDC),
+    // theme: ThemeData(
+    //   useMaterial3: true,
+    //   // Define the default brightness and colors.
+    //   colorScheme: ColorScheme.fromSwatch(
+    //     primarySwatch: Colors.deepPurple,
+    //     accentColor: minhaCorPersonalizada,
+    //   ),
     // ),
+    debugShowCheckedModeBanner: false,
+    home: Menubar(),
   ));
 }
