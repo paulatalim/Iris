7ba56aee64a5e5e556400be8e39e53d144b5aa32
Mariana Aram <marianaaram8@gmail.com>
2023-09-26 10:12:10 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,116 +1,87 @@
+
 import 'package:flutter/material.dart';
+//import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final double peso = 00;
+  final double temperatura = 00;
+  final double altura = 00;
+  final double imc = 00;
+  final Color _iconColorPressed = const Color(0xFFA000FF);
+  final Color _iconColorNotPressed = const Color(0xFF6B86EB);
 
-  // This widget is the root of your application.
+  final List<Color> _iconColor = [
+    const Color(0xFFA000FF),
+    const Color(0xFF6B86EB),
+    const Color(0xFF6B86EB),
+    const Color(0xFF6B86EB)
+  ];
+
+  void _colorSelection(int index) {
+    for (int i = 0; i < _iconColor.length; i++) {
+      if (i != index) {
+        _iconColor[i] = _iconColorNotPressed;
+      } else {
+        _iconColor[i] = _iconColorPressed;
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Iris',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      home: Scaffold(
+        backgroundColor: const Color(0xFFDBC4EF),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CaixaComNumero(texto: ' Peso:                 ' , numero: peso),
+              CaixaComNumero(texto: ' Altura:                ', numero: altura),
+              CaixaComNumero(texto: ' Temperatura:   ', numero: temperatura),
+              CaixaComNumero(texto: ' IMC:                   ', numero: imc),
+            ],
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class CaixaComNumero extends StatelessWidget {
+  final String texto;
+  final double numero;
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  CaixaComNumero({required this.texto, required this.numero});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    //Caixinhas
+    return Container(
+      margin: EdgeInsets.all(30.0), //Espaço entre as caixinhas
+      height: 80.0, // Defina a altura desejada, por exemplo, 100.0 pixels
+      width: 300.0, // Defina a largura desejada, por exemplo, 200.0 pixels
+      decoration: BoxDecoration(
+        color: const Color(0xFFC5A7F1),
+        //border: Border.all(color: Colors.black),
+        borderRadius: BorderRadius.circular(10.0),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween, // Alinha texto e número na mesma linha
+        children: <Widget>[
+          Padding(
+
+            padding: const EdgeInsets.all(8.0),
+            child:Text(
+              '$texto ${numero.toStringAsFixed(2)}', // Exemplo de formatação com 2 casas decimais
+              style: TextStyle(fontSize: 30.0),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
