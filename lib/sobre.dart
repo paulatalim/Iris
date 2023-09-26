ba3ae472988bac12975ffc424252848fb6c37fa4
paula <paulactalim@gmail.com>
2023-09-26 14:24:31 -0300

    adicao de imgs

--- lib/sobre.dart
+++ lib/sobre.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:url_launcher/url_launcher.dart';
+// import 'package:url_launcher/url_launcher.dart';
 
 import 'menu.dart';
 
