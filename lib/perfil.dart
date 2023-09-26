1e05a5dc6b9741332072ad999ec7b6310555d78f
paula <paulactalim@gmail.com>
2023-09-26 01:36:03 -0300

    melhora estrutura de pagina

--- lib/perfil.dart
+++ lib/perfil.dart
@@ -15,7 +15,7 @@ class UserScreen extends StatefulWidget {
 class _UserScreen extends State<UserScreen> {
   TextStyle styleBoxTitle() {
     return const TextStyle(
-        fontSize: 20,
+        fontSize: 25,
         fontWeight: FontWeight.w700,
         letterSpacing: 2,
         color: Color(0xFF373B8A));
@@ -24,33 +24,16 @@ class _UserScreen extends State<UserScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-        centerTitle: true,
-
-        //Menu de voltar ao menu (temporario)
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back_rounded),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const SingIn()),
-            );
-          },
-        ),
-      ),
       body: Container(
-        width: double.infinity, //Largura da tela
-        height: double.infinity,
-        padding: const EdgeInsets.all(10),
+        padding: const EdgeInsets.only(left: 40, right: 40),
 
         //Esqueleto do Corpo
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           crossAxisAlignment: CrossAxisAlignment.center,
           //Corpo
           children: <Widget>[
+            const SizedBox(height: 100),
             //Botão de adição de imagem
             Column(
               crossAxisAlignment: CrossAxisAlignment.center,
@@ -70,36 +53,37 @@ class _UserScreen extends State<UserScreen> {
                     )),
               ],
             ),
-
-            //Campo de texto Nome usuario
+            const SizedBox(height: 70),
             Container(
-              width: double.infinity,
-              child: Text(
-                "*Nome do usuário*",
-                style: GoogleFonts.dosis(
-                  textStyle: styleBoxTitle(),
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                //Campo de texto Nome usuario
+                Text(
+                  "*Nome do usuário*",
+                  style: GoogleFonts.dosis(
+                    textStyle: styleBoxTitle(),
+                  ),
                 ),
-              ),
-            ),
+                const SizedBox(height: 30),
+                //Campo de texto Idade
+                Text(
+                  "Idade: -1",
+                  textAlign: TextAlign.left,
+                  style: GoogleFonts.dosis(
+                    textStyle: styleBoxTitle(),
+                  ),
+                ),
+              ],
+            )),
 
-            //Campo de texto Idade
-            Container(
-              width: double.infinity,
-              child: Text(
-                "Idade: -1",
-                style: GoogleFonts.dosis(
-                  textStyle: styleBoxTitle(),
-                ),
-              ),
+            const SizedBox(
+              height: 70,
             ),
 
             Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Container(
-                  height: 7,
-                ),
-
                 //Botão para edição de conta
                 TextButton(
                   onPressed: () {/*Adicionar rota para função*/},
@@ -120,8 +104,8 @@ class _UserScreen extends State<UserScreen> {
                   ),
                 ),
 
-                Container(
-                  height: 7,
+                const SizedBox(
+                  height: 25,
                 ),
 
                 //Botão para realizar log-off
