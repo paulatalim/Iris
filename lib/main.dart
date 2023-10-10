e1f9a31cf34f28984ac7101d33dcd076500e761e
paula <paulactalim@gmail.com>
2023-10-10 16:27:04 -0300

    adicao de classes teste

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'voices.dart';
 
 import 'menu.dart';
 import 'speech.dart';
+import 'speek.dart';
 
 void main() {
+  // Configuração que permite a utilizacao do app somente com a tela no modo retrato
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  // RecursoDeVoz voice = RecursoDeVoz();
+  // voice.initSpeech();
+  // print(voice.hear());
 
   runApp(MaterialApp(
     title: 'Iris',
@@ -16,7 +22,8 @@ void main() {
           seedColor: const Color(0xFFdba0ff),
         )),
     debugShowCheckedModeBanner: false,
-    // home: const Menubar(),
-    home: SpeechScreen(),
+    home: const Menubar(),
+    // home: SpeechScreen(),
+    // home: Speek(),
   ));
 }
