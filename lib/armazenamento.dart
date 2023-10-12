f55fbdcfdf9470b05e5a2888cb9ab713284a496b
paula <paulactalim@gmail.com>
2023-10-11 22:15:55 -0300

    ajuste e criacao de novos metodos

--- lib/armazenamento.dart
+++ lib/armazenamento.dart
@@ -8,6 +8,7 @@ class Armazenamento {
 
   Armazenamento() {
     _initBD();
+    _recuperarBancoDados();
   }
 
   void _initBD() async {
@@ -16,27 +17,23 @@ class Armazenamento {
     localBancoDados = join(caminhoBancoDados, "banco3.bd");
   }
 
-  _recuperarBancoDados() async {
-    final caminhoBancoDados = await getDatabasesPath();
-    final localBancoDados = join(caminhoBancoDados, "banco3.bd");
-    var bd = await openDatabase(localBancoDados, version: 1,
+  dynamic _recuperarBancoDados() async {
+    bd = await openDatabase(localBancoDados, version: 1,
         onCreate: (db, dbVersaoRecente) {
       String sql =
           "CREATE TABLE usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR, idade INTEGER) ";
       db.execute(sql);
     });
-    return bd;
-    //print("aberto: " + bd.isOpen.toString() );
   }
 
-  _salvarDados(String nome, int idade) async {
+  dynamic _salvarDados(String nome, int idade) async {
     Database bd = await _recuperarBancoDados();
     Map<String, dynamic> dadosUsuario = {"nome": nome, "idade": idade};
     int id = await bd.insert("usuarios", dadosUsuario);
     print("Salvo: $id ");
   }
 
-  _listarUsuarios() async {
+  dynamic _listarUsuarios() async {
     Database bd = await _recuperarBancoDados();
     String sql = "SELECT * FROM usuarios";
     //String sql = "SELECT * FROM usuarios WHERE idade=58";
@@ -46,50 +43,30 @@ class Armazenamento {
     List usuarios =
         await bd.rawQuery(sql); //conseguimos escrever a query que quisermos
     for (var usu in usuarios) {
-      print(" id: " +
-          usu['id'].toString() +
-          " nome: " +
-          usu['nome'] +
-          " idade: " +
-          usu['idade'].toString());
+      print(
+          " id: ${usu['id'].toString()} nome: ${usu['nome']} idade: ${usu['idade'].toString()}");
     }
   }
 
-  _listarUmUsuario(int id) async {
+  dynamic _listarUmUsuario(int id) async {
     Database bd = await _recuperarBancoDados();
     List usuarios = await bd.query("usuarios",
         columns: ["id", "nome", "idade"], where: "id = ?", whereArgs: [id]);
     for (var usu in usuarios) {
-      print(" id: " +
-          usu['id'].toString() +
-          " nome: " +
-          usu['nome'] +
-          " idade: " +
-          usu['idade'].toString());
+      print(
+          " id: ${usu['id'].toString()} nome: ${usu['nome']} idade: ${usu['idade'].toString()}");
     }
   }
-
-  /*_excluirUsuario(int id) async{
-    Database bd = await _recuperarBancoDados();
-    int retorno = await bd.delete(
-        "usuarios",
-        where: "id = ?",  //caracter curinga
-        whereArgs: [id]
-    );
-    print("Itens excluidos: "+retorno.toString());
-  }*/
 
   _excluirUsuario(int id) async {
     Database bd = await _recuperarBancoDados();
     int retorno = await bd.delete("usuarios",
-        // where: "nome = ? AND idade = ?",  //caracter curinga
-        // whereArgs: ["Raquel Ribeiro", 26]
         where: "id = ?", //caracter curinga
         whereArgs: [id]);
     print("Itens excluidos: " + retorno.toString());
   }
 
-  _atualizarUsuario(int id) async {
+  dynamic _atualizarUsuario(int id) async {
     Database bd = await _recuperarBancoDados();
     Map<String, dynamic> dadosUsuario = {
       "nome": "Antonio Pedro",
