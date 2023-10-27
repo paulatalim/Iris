555e1efbbec2e82d134dda06a68542f51674d3ff
paula <paulactalim@gmail.com>
2023-10-27 17:15:43 -0300

    adicao de if

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+// import 'sharedpreference.dart';
+import 'loginmain.dart';
 import 'menu.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  bool isUserLoggedIn = false;
 
   runApp(MaterialApp(
     title: 'Iris',
@@ -15,6 +18,6 @@ void main() {
           seedColor: const Color(0xFFdba0ff),
         )),
     debugShowCheckedModeBanner: false,
-    home: const Menubar(),
+    home: isUserLoggedIn == true ? const Menubar() : const UserLogin(),
   ));
 }
