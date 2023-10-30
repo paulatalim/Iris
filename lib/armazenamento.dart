e2be6f71a48d8745a055898fc0211388bcaeb12d
Yago Garzon Chaves <114442039+yagogarzon@users.noreply.github.com>
2023-10-30 14:33:28 -0300

    Simplificando código

--- lib/armazenamento.dart
+++ lib/armazenamento.dart
@@ -41,17 +41,11 @@ class Armazenamento {
           FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
         );''';
 
-    // Abre e cria as tabelas
-    database = await openDatabase(localBancoDados, version: 2, // Aumente a versão para refletir a mudança no esquema
-        onCreate: (database, databaseVersaoRecente) {
-      database.execute(conta);
-      database.execute(infoAdicional);
-    });
-
     // Abre e cria a tabela
     database = await openDatabase(localBancoDados, version: 1,
         onCreate: (database, databaseVersaoRecente) {
       database.execute(conta);
+      database.execute(infoAdicional);
     });
   }
 
