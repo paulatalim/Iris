bc4423cda52e1c948ac0461210c879b98761ad1f
Pedro Mafra <pedro.m.vas@hotmail.com>
2023-10-29 22:15:46 -0300

    Criação de classe do usuario

---
+++ lib/usuario.dart
@@ -0,0 +1,18 @@
+import 'dart:core';
+import 'dart:js_util';
+
+/*
+* Classe usuario contendo variáveis básicas para o utilização no app
+*/
+class User {
+  late String nome;
+  late String sobrenome;
+  late String id;
+  late String email;
+  late String peso;
+  late String altura;
+
+  User(){
+    User(); //Criação de novo objeto
+  }
+}
