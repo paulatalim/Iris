11c72c37f7066f90bfc2a2bf55cbbfca4c0f0401
paula <paulactalim@gmail.com>
2023-10-31 16:00:09 -0300

    carregamento de info

--- lib/sharedpreference.dart
+++ lib/sharedpreference.dart
@@ -1,11 +1,41 @@
 import 'package:shared_preferences/shared_preferences.dart';
+import 'usuario.dart';
+import 'armazenamento.dart';
 
-void setUserLoggedIn(bool value) async {
+void setUserLoggedIn(String value) async {
   final prefs = await SharedPreferences.getInstance();
-  prefs.setBool('loggedIn', value);
+  prefs.setString('email', value);
 }
 
 Future<bool> isUserLoggedIn() async {
+  Armazenamento storage = Armazenamento();
   final prefs = await SharedPreferences.getInstance();
-  return Future.value(prefs.getBool('loggedIn') ?? false);
+  String email = prefs.getString('email') ?? '';
+
+  // Recupera os dados do usuario
+  if (email.compareTo('') != 0) {
+    print(email);
+
+    List dados = await storage.buscarUsuario(email);
+
+    // Verifica se o registro exite
+    if (dados.isEmpty) {
+      return Future.value(false);
+    }
+
+    usuario.id = dados[0]["id"]; //Carregando o ID
+    usuario.nome = dados[0]["nome"]; //Carregando nome
+    usuario.sobrenome = dados[0]["sobrenome"];
+    usuario.email = email;
+
+    dados = await storage.buscarInfoAdicional(usuario.id);
+    usuario.peso = dados[0]["peso"];
+    usuario.altura = dados[0]["altura"];
+    usuario.temperatura = dados[0]["temperatura"];
+    usuario.imc = dados[0]["imc"];
+
+    return Future.value(true);
+  }
+
+  return Future.value(false);
 }
