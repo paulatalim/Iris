5860b67a9a09bb2bc14d1e0d442976fec3e5e56a
paulatalim <paulactalim@gmail.com>
2023-11-18 12:28:14 -0300

    atualizacao de nome de classe e biblioteca

--- lib/interfaces/sobre.dart
+++ lib/interfaces/sobre.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
-// import 'package:url_launcher/url_launcher.dart';
 
 import 'menu.dart';
 
@@ -70,7 +69,7 @@ class _SobreState extends State<Sobre> {
                             Navigator.push(
                               context,
                               MaterialPageRoute(
-                                  builder: (context) => const Menubar()),
+                                  builder: (context) => const Menu()),
                             ),
                           );
                         }),
