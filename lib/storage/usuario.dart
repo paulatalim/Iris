5913cf4c52f23987fc4506b94d6d82e732a5d814
paulatalim <paulactalim@gmail.com>
2023-11-18 12:27:20 -0300

    criacao de metodo para importar dados do banco de dados

--- lib/storage/usuario.dart
+++ lib/storage/usuario.dart
@@ -1,4 +1,5 @@
 import 'dart:core';
+import 'armazenamento.dart';
 
 /*
 * Classe usuario contendo variáveis básicas para o utilização no app
@@ -19,6 +20,24 @@ class User {
     _altura = 0;
     _imc = 0;
     _temperatura = 0;
+  }
+
+  /// Busca os dados no banco de dados atraves do [email] e os salva na memoria principal
+  void importarDados (String email) async {
+    Armazenamento storage = Armazenamento();
+
+    // Recupera os dados do usuario
+    List dados = await storage.buscarUsuario(email);
+    id = dados[0]["id"]; //Carregando o ID
+    nome = dados[0]["nome"]; //Carregando nome
+    sobrenome = dados[0]["sobrenome"];
+    email = email;
+
+    Map infoAdicionais = await storage.buscarInfoAdicional(id);
+    peso = infoAdicionais["peso"];
+    altura = infoAdicionais["altura"];
+    temperatura = infoAdicionais["temperatura"];
+    imc = infoAdicionais["imc"];
   }
 
   get peso => _peso;
