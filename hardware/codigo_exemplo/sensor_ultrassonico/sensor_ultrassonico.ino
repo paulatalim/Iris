f6f5ea832744cb1583633a16800373bdc1073795
paulatalim <paulactalim@gmail.com>
2023-12-03 21:28:47 -0300

    criacao de codigo exemplo para o mqtt

--- hardware/codigo_exemplo/sensor_ultrassonico/sensor_ultrassonico.ino
+++ hardware/codigo_exemplo/sensor_ultrassonico/sensor_ultrassonico.ino
@@ -1,21 +1,12 @@
-#include "LiquidCrystal_I2C.h"
-#include "Wire.h" 
-#include "Ultrasonic.h"
+#include <Ultrasonic.h>
 
-LiquidCrystal_I2C lcd(0x20,16,2);   //Configura o display
-HC_SR04 sensor1(12,13);             //Configura os pinos sensor ultrassonico (Trigger,Echo)
+HC_SR04 sensor1(12,14); // (trigger, echo)
 
 void setup() {
-  lcd.init();                       // Inicializa o LCD
-  lcd.backlight();                  // Liga o backlight do LCD
+  Serial.begin(9600);
 }
 
 void loop() {
-  lcd.print("      ");
-  lcd.print(sensor1.distance());    //Exibe no display as medidas
-  lcd.print(" cm");
+  Serial.println(sensor1.distance());
   delay(100);
-  lcd.clear();                      //Apaga o que foi escrito no display
-}  // put your setup code here, to run once:
-
 }
