be8711b82e1028103a3f0f7025c44fbf34cca319
Pedro Mafra <pedro.m.vas@hotmail.com>
2023-12-05 21:38:09 -0300

    Create auth_gate.dart
    Arquivo utilizado para determinar se usuário se encontra logado ou não

---
+++ lib/auth_gate.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:iris_app/loginmain.dart';
+
+import 'home.dart';
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return UserLogin();
+        }
+
+        return const HomeScreen();
+      },
+    );
+  }
+}
+//Link da página do Firebase ao respeito: https://firebase.google.com/codelabs/firebase-auth-in-flutter-apps?hl=pt-br#3
