444700ef9725006a4dd1ef353bd2cb04ad036b73
paulatalim <paulactalim@gmail.com>
2024-01-04 20:49:06 -0300

    adicao de codigo para analise e mecanismos de entrada de dados

--- lib/hardware/bluetooth/bluetooth_manager.dart
+++ lib/hardware/bluetooth/bluetooth_manager.dart
@@ -1,10 +1,23 @@
 import 'bluetooth.dart';
+
 import '../../storage/usuario.dart';
+import '../available_devices.dart';
 
 class BluetoothManager {
   late String nomeDispositivo;
   late Bluetooth bluetooth;
   String? _state;
+  String? _stateAltura;
+  String? _statePeso;
+  String? _stateTemperatura;
+  String? _time;
+
+  int cronometroAltura = 30;
+  int cronometroPeso = 30;
+  int cronometroTemp = 30;
+
+  bool salvarPeso = false;
+  bool salvarTemperatura = false;
 
   BluetoothManager() {
     nomeDispositivo = "Iris Hardware";
@@ -12,12 +25,92 @@ class BluetoothManager {
     atualizarStatus();
   }
 
+  void medirAltura() async{
+    if(_stateAltura != null) {
+      await bluetooth.publish("c");
+
+      // Esperar um tempo
+      while(cronometroAltura != 0) {
+        _stateAltura = "Fique debaixo do sensor";
+        _time = "Tempo para medir: $cronometroAltura s";
+
+        cronometroAltura --;
+        await Future.delayed(const Duration(seconds: 1));
+      }
+
+      _time = null;
+      cronometroAltura = 30;
+
+      _stateAltura = "Processando ...";
+
+      await bluetooth.publish("a");
+    }
+  }
+
+  void medirPeso() async {
+    if(_statePeso != null) {
+      while(cronometroPeso != 0) {
+        _statePeso = "Suba na balança\nTempo para medir: $cronometroPeso s";
+        _time = "Tempo para medir: $cronometroPeso s";
+
+        cronometroPeso --;
+        await Future.delayed(const Duration(seconds: 1));
+      }
+
+      _time = null;
+      cronometroPeso = 30;
+      _statePeso = "Processando ...";
+
+      salvarPeso = true;
+    }
+  }
+
+  void medirTemperatura() async {
+    // Verifica se o sistema está conectado
+    if(_stateTemperatura != null) {
+      while(cronometroTemp != 0) {
+        _stateTemperatura = "Coloque o sensor debaixo do braço";
+        _time = "Tempo para medir: $cronometroTemp s";
+
+        cronometroTemp --;
+        await Future.delayed(const Duration(seconds: 1));
+      }
+
+      _time = null;
+      cronometroTemp = 30;
+      _stateTemperatura = "Processando ...";
+
+      salvarTemperatura = true;
+    }
+  }
+
   void atualizarDados() async {
     while (true) {
-      if (bluetooth.msgBT.trim().isNotEmpty) {
-        usuario.temperatura = double.parse(bluetooth.msgBT);
+      String msg = bluetooth.msgBT;
+
+      if (msg.trim().isNotEmpty) {        
+        switch (msg[0]) {
+          case 'T':
+            if(salvarTemperatura) {
+              usuario.temperatura = double.parse(msg.substring(1));
+              _stateTemperatura = 'Concluído';
+              salvarTemperatura = false;
+            }
+            break;
+          case 'A':
+            usuario.altura = double.parse(msg.substring(1));
+            _stateAltura = "Concluído";
+            break;
+          case 'P':
+            if (salvarPeso) {
+              usuario.peso = double.parse(msg.substring(1));
+              _statePeso = "Concluído";
+              salvarPeso = false;
+            }
+            break;
+        }
       } 
-      await Future.delayed(const Duration(milliseconds: 2000));
+      await Future.delayed(const Duration(milliseconds: 1000));
     }
   }
 
@@ -30,6 +123,7 @@ class BluetoothManager {
         if (!bluetooth.isDiscovering) {
           // Atualiza os status para permissao concedida
           _state = "Permissão concedida";
+
         } else {
           // Atualiza os status para encontrando um dispositivo
           _state = "Procurando Dispositivo";
@@ -38,14 +132,24 @@ class BluetoothManager {
         // Enquanto não estiver permitido o bluetooth
         _state = "Inicializando bluetooth";
       }
+      
+      // Atualiza status do sistema
+      dispositivo[1].status = _state ?? "Status";
 
       // Aguarda 2 segundos até a proxima verificacao de status
       await Future.delayed(const Duration(milliseconds: 1000));
     }
 
     _state = "Conectado";
+    _stateTemperatura = 'Processando ...';
+    _stateAltura = 'Calibrando sensor...';
+    _statePeso = 'Processando ...';
     atualizarDados();
   }
 
   get state => _state ?? "Bluetooth não inicializado";
+  get statePeso => _statePeso ?? "Conectando ...";
+  get stateAltura => _stateAltura ?? "Conectando ...";
+  get stateTemperatura => _stateTemperatura ?? "Conectando ...";
+  get time => _time;
 }
