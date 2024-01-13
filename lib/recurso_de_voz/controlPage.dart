6be63fe843bc22a6a7b52c208d13a949e8c8513b
Mariana Aram <marianaaram8@gmail.com>
2024-01-12 22:48:16 -0300

    Update controlPage.dart
    Tela de carregamento pronta

--- lib/recurso_de_voz/controlPage.dart
+++ lib/recurso_de_voz/controlPage.dart
@@ -4,14 +4,21 @@ import 'package:provider/provider.dart';
 import '../mqtt/state/MQTTAppState.dart';
 import '../interfaces/menu.dart';
 
+
+import '../recurso_de_voz/controlPage.dart';
+import '../recurso_de_voz/voices.dart';
+import '../storage/usuario.dart';
+
+
+//StatefulWidget índice como parâmetro.
 class ControlScreen extends StatefulWidget {
   ControlScreen({super.key, required this.index});
   int index;
-
   @override
   State<ControlScreen> createState() => _ControlScreenState();
 }
 
+//Estado de carregamento 
 class _ControlScreenState extends State<ControlScreen> {
   bool isLoading = true;
 
@@ -21,6 +28,7 @@ class _ControlScreenState extends State<ControlScreen> {
     _irMenu(widget.index);
   }
 
+  //Metodo para esperar 2 segundos e definir Isloading como falso
   void _irMenu(int index) async {
     debugPrint(index.toString());
     await Future.delayed(const Duration(seconds: 2));
@@ -30,24 +38,41 @@ class _ControlScreenState extends State<ControlScreen> {
     _alterarUI(index);
   }
 
+  //Metodo para mudar a tela de acordo com o index
   void _alterarUI(int index) {
+
+    voice.speek("Carregando, aguarde!"); //Voz carregando 
+
     Navigator.push(
       context,
       MaterialPageRoute(
         builder: (context) => ChangeNotifierProvider<MQTTAppState>(
           create: (_) => MQTTAppState(),
+
+          //Scaffold
           child: Scaffold(
-            appBar: AppBar(
-              title: Text('Carregando'),
-              backgroundColor: Colors.purple, // Cor lilás
-            ),
+
             body: Container(
               color: Colors.purple, // Cor lilás
               child: Center(
-                child: CircularProgressIndicator(
+                child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Carregando...',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 16.0), // Espaço entre o texto e o indicador de carregamento
+                  CircularProgressIndicator(
                   valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
                 ),
+                ],
               ),
+            ),
             ),
           ),
         ),
@@ -55,13 +80,10 @@ class _ControlScreenState extends State<ControlScreen> {
     );
   }
 
+  //Metodo para retornar o scaffold criado com base no estado de Isloading
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Minha Página'),
-        backgroundColor: Colors.purple, // Cor lilás
-      ),
       body: isLoading
           ? Center(
               child: CircularProgressIndicator(
@@ -70,7 +92,6 @@ class _ControlScreenState extends State<ControlScreen> {
             )
           : Container(
               color: Colors.purple, // Cor lilás
-              // Adicione o conteúdo da sua página aqui
             ),
     );
   }
