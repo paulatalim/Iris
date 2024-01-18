c410633f651eb5f2301f341b36498ab71e044c85
paulatalim <paulactalim@gmail.com>
2024-01-18 15:36:05 -0300

    debug code

---
+++ hardware/iris_hardware/Altura.hpp
@@ -0,0 +1,118 @@
+#include <Ultrasonic.h>
+
+#define PIN_HCSR04_ECHO 18
+#define PIN_HCSR04_TRIG 19
+
+Ultrasonic ultrasonic(PIN_HCSR04_TRIG, PIN_HCSR04_ECHO);
+
+class Altura {
+private:
+  float altura_calibracao;
+  float altura_usuario;
+  bool isCalibrated;
+  long envioAnterior;
+  long time;
+
+  void setup_hcsr04() {
+    pinMode(PIN_HCSR04_ECHO, INPUT);  // DEFINE O PINO COMO ENTRADA (RECEBE)
+    pinMode(PIN_HCSR04_TRIG, OUTPUT); // DEFINE O PINO COMO SAIDA (ENVIA)
+  }
+
+  /**
+  * @brief Calcula a distancia entre o sensor e o ponto mais proximo
+  * 
+  * @return inteiro: altura em metros
+  */
+  float hcsr04() {
+    float distancia;
+
+    digitalWrite(PIN_HCSR04_TRIG, LOW);
+    delayMicroseconds(2);
+    digitalWrite(PIN_HCSR04_TRIG, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(PIN_HCSR04_TRIG, LOW);
+    distancia = float(ultrasonic.read(CM));
+    distancia /= 100;
+
+    return distancia;
+  }
+
+  void atualizar_tempo_calibracao(bool reiniciar_contagem) {
+    if(reiniciar_contagem) {
+      this->envioAnterior = millis();
+      this->time = 10000;
+    } else {
+      this->time -= long(millis()) - this->envioAnterior;
+    }
+  }
+
+public:
+  Altura () {
+    this->altura_calibracao = 0;
+    this->altura_usuario = 0;
+    this->isCalibrated = false;
+    this->envioAnterior = 0;
+    this->time = 10000;
+
+    setup_hcsr04();
+  }
+
+  void calibrar_sensor() {
+    float altura_anterior;
+
+    if(long(time) > 0) {
+      altura_anterior = this->altura_calibracao;
+      this->altura_calibracao = hcsr04();
+
+      if(altura_anterior == this->altura_calibracao) {
+        atualizar_tempo_calibracao(false);  
+      } else {
+        atualizar_tempo_calibracao(true);
+      }
+    } else {
+      this->isCalibrated = true;
+    }
+  }
+
+  void medir() {
+    float altura =  hcsr04();
+    Serial.println(altura);
+    Serial.println(this->altura_calibracao);
+    this->altura_usuario = this->altura_calibracao - altura;
+  }
+
+  void imprimir() {
+    if (isCalibrated) {
+      // Imprimi altura do usuario
+      Serial.print("Altura: ");
+      Serial.print(this->altura_usuario);
+      Serial.println(" m");
+    } else {
+      // Imprimi altura de calibracao
+      Serial.print("Altura de calibracao: ");
+      Serial.print(this->altura_calibracao);
+      Serial.println(" m");
+    }
+  }
+
+  long get_timeCalibracao(){
+    return this->time;
+  }
+
+  float get_altura() {
+    return this->altura_usuario;
+  }
+
+  void set_isCalibrated(bool value) {
+    isCalibrated = value;
+
+    // Reinicia valores
+    if (!value) {
+      altura_usuario = 0;
+    }
+  }
+
+  bool get_isCalibrated() {
+    return this->isCalibrated;
+  }
+};
