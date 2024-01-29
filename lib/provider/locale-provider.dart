466ce786654c75f9e7b259655ac1b181efd53bb0
paulatalim <paulactalim@gmail.com>
2024-01-29 19:15:15 -0300

    adicao de sistema para salvar idioma

--- lib/provider/locale-provider.dart
+++ lib/provider/locale-provider.dart
@@ -1,8 +1,11 @@
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
+
 import '../l10n/l10n.dart';
 
 class LocaleProvider extends ChangeNotifier {
   Locale? _locale;
+  bool atualizado = false;
 
   Locale get locale => _locale ?? const Locale('en');
 
@@ -16,5 +19,19 @@ class LocaleProvider extends ChangeNotifier {
   void clearLocale() {
     _locale = null;
     notifyListeners();
+  }
+
+  void saveLocale(Locale locale) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString('idioma', locale.languageCode);
+  }
+
+  void getLocale() async {
+    if (!atualizado) {
+      final prefs = await SharedPreferences.getInstance();
+      _locale = Locale(prefs.getString('idioma')??'en');
+      notifyListeners();
+      atualizado = true;
+    }
   }
 }
